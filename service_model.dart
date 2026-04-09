0a099f009170c73625311cdc35fc369684933f1d
tranxuanquan <tranxuanquan101204@gmail.com>
2026-04-09 17:11:46 +0700

    Delete service_model.dart

--- service_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ServiceModel {
-  final String id;
-  final String title;
-  final IconData icon;
-
-  ServiceModel({required this.id, required this.title, required this.icon});
-}
